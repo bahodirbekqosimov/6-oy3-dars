a67d026b70dc2614f5780d90dfff107598389e4b
Bahodirbek <bahodirbek20090405@gmail.com>
2025-12-04 18:14:14 +0500

    detail and base.html add

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from . import views
+from .views import HomeView, DetatilView
 
 urlpatterns = [
     
-    path('',views.home, name='home'),
+    path('',HomeView.as_view(), name='home'),
+    path("detail/<int:id>/",DetatilView.as_view(),name="detail")
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,53 @@
 from django.shortcuts import render
+from .models import Article
+from django.views import View
 
-def home(req):
-    return render(req,'main/index.html')
+
+
+class HomeView(View):
+    def get(self,req):
+        articles = Article.objects.all().filter(is_active = True).order_by("-created_at")[:3]
+        data = {
+            "articles" : articles
+        }
+        return render(req,'main/index.html',context=data)
+    
+
+
+# def home(req):
+#     articles = Article.objects.all().filter(is_active = True).order_by("-created_at")[:3]
+#     data = {
+#         "articles" : articles
+#     }
+
+    
+#     return render(req,'main/index.html',context=data)
+class DetatilView(View):
+    def get(self,req,id):
+        article = Article.objects.get(id = id)
+        if article:
+            data = {
+                'article':article
+            }
+        else:
+            data = {
+                'article':False
+            }
+        
+        return render(req,"main/detail.html",context=data)
+            
+            
+
+def detail(req,id):
+    article = Article.objects.get(id = id)
+    if article:
+        data = {
+            'article':article
+        }
+    else:
+        data = {
+            'article':False
+        }
+    
+    return render(req,"main/detail.html",context=data)
+        
