aca7a3d9619fae4dcd7e1bf3d8635a077dcb5878
Bahodirbek <bahodirbek20090405@gmail.com>
2025-12-05 09:58:10 +0500

    add 404

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.views import View
 
@@ -24,30 +24,40 @@ class HomeView(View):
 #     return render(req,'main/index.html',context=data)
 class DetatilView(View):
     def get(self,req,id):
-        article = Article.objects.get(id = id)
-        if article:
+        try:
+            article = Article.objects.get(id = id)
+  
             data = {
-                'article':article
+                'article':article,
+                "title": article.title
             }
-        else:
-            data = {
-                'article':False
-            }
-        
-        return render(req,"main/detail.html",context=data)
+            return render(req,"main/detail.html",context=data)
+        except:
+            return render(req,"main/404.html")
             
             
 
-def detail(req,id):
-    article = Article.objects.get(id = id)
-    if article:
-        data = {
-            'article':article
-        }
-    else:
-        data = {
-            'article':False
-        }
+# def detail(req,id):
+#     article = Article.objects.get(id = id)
+#     if article:
+#         data = {
+#             'article':article
+#         }
+#     else:
+#         data = {
+#             'article':False
+#         }
     
-    return render(req,"main/detail.html",context=data)
+#     return render(req,"main/detail.html",context=data)
         
+        
+
+class ArticlesView(View):
+    def get(self,req):
+        articles =  Article.objects.all()
+        
+        data = {
+            "articles" : articles
+        }
+        
+        return render(req,"articles.html",context=data)
